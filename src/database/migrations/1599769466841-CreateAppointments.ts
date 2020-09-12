c6afb42df36e7228fd7df3ca2223eacbdad86fcf
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-09-12 18:24:00 +0100

    Foreign Key created into appointment table

--- src/database/migrations/1599769466841-CreateAppointments.ts
+++ src/database/migrations/1599769466841-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1599769466841 implements MigrationInterfa
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
