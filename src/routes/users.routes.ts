36e6c7c59b4cd8074ce5c97f4aba7e4af8fff44e
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-09-16 20:41:20 +0100

    Upload avatar image added

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
 
@@ -28,8 +33,22 @@ usersRouter.post('/', async (request, response) => {
 });
 
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
-    return response.json({ ok: true });
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+    try {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename,
+        })
+
+        delete user.password;
+
+        return response.json(user);
+
+    } catch (error) {
+        return response.status(400).json({error: error.message})
+    }
 });
 
 export default usersRouter;
