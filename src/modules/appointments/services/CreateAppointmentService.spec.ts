38edfe745a81402a2ec45b43169a9e53a425f104
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-10-27 15:53:57 +0000

    Mock Repository created

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,18 @@
+import MockAppointmentRepository from '@modules/appointments/repositories/mocks/MockAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-    it('should be able to create a new appointment', () => {
-       
+    it('should be able to create a new appointment', async () => {
+        const mockAppointmentRepository = new MockAppointmentRepository();
+        const createAppointmentRepository = new CreateAppointmentService(mockAppointmentRepository);
+
+        const appointment = await createAppointmentRepository.execute({
+            date: new Date(),
+            provider_id: '123456789'
+        });
+
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('123456789');
     });
 
     // it('should not be able to create two appointments on the same time', () => {
