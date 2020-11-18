d4e6ffcecc5994d8a38d85c4c8720199ae864d38
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-11-18 12:01:55 +0000

    Dependency injection configuration updated

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,18 +1,10 @@
-import {container} from 'tsyringe';
-import mailConfig from '@config/mail';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-import IMailProvider from './MailProvider/models/IMailProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
-import IStorageProvider from './StorageProviders/models/IStorageProvider';
+import './StorageProviders';
+import './MailTemplateProvider';
+import './MailProvider';
 
-container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProvider);
-container.registerSingleton<IMailTemplateProvider>('MailTemplateProvider', HandlebarsMailTemplateProvider);
-container.registerInstance<IMailProvider>('MailProvider',
-mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider));
+
+
+
+
 
 
