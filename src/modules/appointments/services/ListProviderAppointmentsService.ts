b1fb85339af01f7ed257752d215a63fc3180312c
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-11-19 19:36:45 +0000

    Serializing appointments in the cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import {injectable, inject} from 'tsyringe';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -38,7 +39,7 @@ class ListProviderAppointmentsService {
                 year
             });
 
-            await this.cacheProvider.save(cacheKey, appointments);
+            await this.cacheProvider.save(cacheKey, classToClass(appointments));
         }
         return appointments;
     }
