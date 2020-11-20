0208433e996076d8a3f358134c94eab332bb89b4
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-11-20 10:11:00 +0000

    Show url to recovery password for dev environment

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -41,6 +41,10 @@ export default class EtherealMailProvider implements IMailProvider{
             subject,
             html: await this.mailTemplateProvider.parse(templateData),
         });
+
+        console.log('Message sent: %s', message.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+
     };
 
 }
